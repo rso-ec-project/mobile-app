596bbe24d13428054f9702f29033f310e894853a
Anzx <31044008+anzetomazin@users.noreply.github.com>
2021-12-28 14:20:36 +0100

    Create a list of user reservations

--- src/lib/screens/user_reservations.dart
+++ src/lib/screens/user_reservations.dart
@@ -1,4 +1,8 @@
+import 'package:charging_stations_mobile/models/reservation.dart';
+import 'package:charging_stations_mobile/services/reservation_service.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../config.dart';
 
 class UserReservationsScreen extends StatefulWidget {
   const UserReservationsScreen({Key? key}) : super(key: key);
@@ -8,13 +12,52 @@ class UserReservationsScreen extends StatefulWidget {
 }
 
 class _UserReservationsScreenState extends State<UserReservationsScreen> {
+
+  late Future<List<Reservation>> futureReservations;
+
+  @override
+  void initState() {
+    super.initState();
+    futureReservations = ReservationService.getReservationsAsync(Config.userId);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("My Reservations"),
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder <List<Reservation>>(
+              future: futureReservations,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  List<Reservation> data = snapshot.data as List<Reservation>;
+                  return
+                    ListView.builder(
+                        itemCount: data.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          var reservation = data[index];
+                          return ListTile(
+                            title: Text(DateFormat('dd.MM.yyyy hh:mm').format(reservation.from)),
+                            onTap: () {
+
+                            },
+                          );
+                        }
+                    );
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+                // By default show a loading spinner.
+                return const Center(child: CircularProgressIndicator());
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- src/lib/services/reservation_service.dart
+++ src/lib/services/reservation_service.dart
@@ -29,6 +29,21 @@ class ReservationService {
     }
   }
 
+  static Future<List<Reservation>> getReservationsAsync(int userId) async {
+    var requestString = Config.resUrl + '/Reservations';
+    requestString += '?userId=' + userId.toString();
+
+    final response = await http.get(Uri.parse(requestString));
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      print(response.body);
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((data) => Reservation.fromJson(data)).toList();
+    } else {
+      throw Exception('Unexpected error occured!');
+    }
+  }
+
   static Future<HttpClientResponse> postAsync(ReservationPost reservationPost) async {
     HttpClientRequest request = await client.postUrl(Uri.parse(Config.resUrl + '/Reservations'));
     request.headers.set('Content-Type', 'application/json');
