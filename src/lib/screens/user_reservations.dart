c75635ed9768abe97b096c131ea207b7c28873bd
Anzx <31044008+anzetomazin@users.noreply.github.com>
2021-12-28 14:27:23 +0100

    Add toggle buttons to filter by StatusId

--- src/lib/screens/user_reservations.dart
+++ src/lib/screens/user_reservations.dart
@@ -14,7 +14,7 @@ class UserReservationsScreen extends StatefulWidget {
 class _UserReservationsScreenState extends State<UserReservationsScreen> {
 
   late Future<List<Reservation>> futureReservations;
-
+  var isSelected = [true, false, false];
   @override
   void initState() {
     super.initState();
@@ -29,12 +29,36 @@ class _UserReservationsScreenState extends State<UserReservationsScreen> {
       ),
       body: Column(
         children: [
+          SizedBox(
+            height: 50,
+            child: ToggleButtons(
+              children: const <Widget>[
+                Text("In Progress"),
+                Text("Cancelled"),
+                Text("Closed"),
+              ],
+              onPressed: (int index) {
+                setState(() {
+                  for (int buttonIndex = 0; buttonIndex < isSelected.length; buttonIndex++) {
+                    if (buttonIndex == index) {
+                      isSelected[buttonIndex] = true;
+                    } else {
+                      isSelected[buttonIndex] = false;
+                    }
+                  }
+                });
+              },
+              isSelected: isSelected,
+            ),
+          ),
           Expanded(
             child: FutureBuilder <List<Reservation>>(
               future: futureReservations,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   List<Reservation> data = snapshot.data as List<Reservation>;
+                  var selectedStatus = isSelected.indexOf(true) + 1;
+                  data = data.where((element) => element.statusId == selectedStatus).toList();
                   return
                     ListView.builder(
                         itemCount: data.length,
